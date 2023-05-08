5ecb03cdc806a8d57b284cef58794171acf7ba9c
Agustin101 <agus_101@live.com.ar>
2023-05-08 12:53:34 -0300

    Exercise 'Unicafe' finished

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const StatisticLine = ({ amount, text }) => (
+    <tr>
+        <td>{text}</td>
+        <td>{amount}</td>
+    </tr>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Statistics = ({ good, bad, neutral, total }) => {
+    const average = total > 0 ? (good - bad) / total : 0;
+    const positives = total > 0 ? (good / total) * 100 : 0;
+
+    return (
+        <>
+            <h2>Statistics</h2>
+            <table>
+                <tbody>
+                    <StatisticLine amount={good} text="Good" />
+                    <StatisticLine amount={neutral} text="Neutral" />
+                    <StatisticLine amount={bad} text="Bad" />
+                    <StatisticLine amount={total} text="All" />
+                    <StatisticLine amount={average} text="Average" />
+                    <StatisticLine amount={positives} text="Positive" />
+                </tbody>
+            </table>
+        </>
+    );
+};
+
+const NoFeedBackPlaceholder = () => <h2>No feedback given.</h2>;
+
+const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+    const total = good + neutral + bad;
+
+    const handleGood = () => setGood(good + 1);
+    const handleNeutral = () => setNeutral(neutral + 1);
+    const handleBad = () => setBad(bad + 1);
+
+    return (
+        <div>
+            <h1>Give feedback</h1>
+            <Button text="Good" handler={handleGood} />
+            <Button text="Neutral" handler={handleNeutral} />
+            <Button text="Bad" handler={handleBad} />
+            {total === 0 ? (
+                <NoFeedBackPlaceholder />
+            ) : (
+                <Statistics
+                    good={good}
+                    bad={bad}
+                    neutral={neutral}
+                    total={total}
+                />
+            )}
+        </div>
+    );
+};
+
+const Button = ({ text, handler }) => {
+    return <button onClick={handler}>{text}</button>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
